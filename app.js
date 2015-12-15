38863fbeebb54dc1b279c499d25ceaccc19bba76
Drewster317 <drewster317@gmail.com>
2015-12-15 15:04:15 -0500

    CREATED a new model called posts and required mongoose in app.js

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/news');
+
+require('./models/Comments');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
