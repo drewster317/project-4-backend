31c0649ad5b05212c733c4d490f5acda169008de
Drewster317 <drewster317@gmail.com>
2015-12-15 15:21:59 -0500

    CREATED a new model for comments and required them in the app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-
 mongoose.connect('mongodb://localhost/news');
 
+require('./models/Posts');
 require('./models/Comments');
 
 var routes = require('./routes/index');
