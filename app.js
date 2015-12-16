c690694dcf2417047532e1d9728c311260e97344
Drewster317 <drewster317@gmail.com>
2015-12-16 18:13:08 -0500

    ADD passport to app

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 mongoose.connect('mongodb://localhost/news');
 
 require('./models/Posts');
 require('./models/Comments');
 require('./models/Users')
+require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,6 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
