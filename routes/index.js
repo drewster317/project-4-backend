d5a0ffff7ef99b71f295e81af196f43ca264c279
Drewster317 <drewster317@gmail.com>
2015-12-16 09:03:59 -0500

    IMPLEMENTED a new route and a middleware function to retrieve comments

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,9 @@ router.param('post', function(req, res, next, id) {
 });
 
 router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+  req.post.populate('comments', function(err,post) {
+    res.json(post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -49,6 +51,28 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     if (err) { return next(err); }
 
     res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req,res,next) {
+  req.comment.upvote(function(err,comment) {
+    if (err) { return next(err); }
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
   });
 });
 
