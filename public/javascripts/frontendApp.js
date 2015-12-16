738d579a72827ff38603a9f908926aa7cf212361
Drewster317 <drewster317@gmail.com>
2015-12-16 10:40:12 -0500

    ADDED methods to add comments and upvote comments

--- public/javascripts/frontendApp.js
+++ public/javascripts/frontendApp.js
@@ -8,13 +8,23 @@ app.config([
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      }
     })
 
     .state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
-      controller: 'PostsCtrl'
+      controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
     });
 
     $urlRouterProvider.otherwise('home');
@@ -22,11 +32,47 @@ app.config([
   }])
 
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
     posts: []
   };
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
+    });
+  };
+
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+    .success(function(data){
+      post.upvotes += 1;
+    });
+  };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
+
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function(data) {
+      comment.upvotes += 1;
+    })
+  };
+
   return o;
+
 }])
 
 app.controller('MainCtrl', [
@@ -39,13 +85,9 @@ app.controller('MainCtrl', [
 
     $scope.addPost = function() {
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Drew', body: 'Nice post!', upvotes: 0},
-        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -53,27 +95,34 @@ app.controller('MainCtrl', [
     }
 
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    }
+      posts.upvote(post);
+    };
 
   }])
 app.controller('PostsCtrl', [
   '$scope',
   '$stateParams',
   'posts',
-  function($scope, $stateParams, posts){
-    $scope.post = posts.posts[$stateParams.id];
+  'post',
+  function($scope, $stateParams, posts, post){
+    $scope.post = post;
 
-    $scope.addComment = function() {
+    $scope.addComment = function(){
       if($scope.body === '') { return; }
-      $scope.post.comments.push({
+      posts.addComment(post._id, {
         body: $scope.body,
         author: 'user',
-        upvotes: 0
+      }).success(function(comment) {
+        $scope.post.comments.push(comment);
       });
-
       $scope.body = '';
     }
+
+    $scope.incrementUpvotes = function(comment) {
+      posts.upvoteComment(post, comment);
+    }
+
+
 
   }]);
 
