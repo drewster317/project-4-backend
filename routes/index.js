6883fd0c7d74d6439c467f5e1df575e6f9021f96
Drewster317 <drewster317@gmail.com>
2015-12-16 17:49:53 -0500

    reworked routes folder back to where app was working

--- routes/index.js
+++ routes/index.js
@@ -43,9 +43,9 @@ router.get('/posts', function(req, res, next) {
 });
 
 
-router.post('/posts', auth, function(req, res, next) {
+router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
-  post.author = req.payload.username;
+  //post.author = req.payload.username;
 
   post.save(function(err, post){
     if(err){ return next(err); }
@@ -62,7 +62,7 @@ router.param('post', function(req, res, next, id) {
     if (err) { return next(err); }
     if (!post) { return next(new Error('can\'t find post')); }
 
-    // req.comment = comment;
+    req.post = post;
     return next();
   });
 });
@@ -90,7 +90,7 @@ router.get('/posts/:post', function(req, res) {
 });
 
 //upvote post
-router.put('/posts/:post/upvote', auth, function(req, res, next) {
+router.put('/posts/:post/upvote', function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -100,10 +100,10 @@ router.put('/posts/:post/upvote', auth, function(req, res, next) {
 
 //create a comment
 
-router.post('/posts/:post/comments', auth, function(req, res, next) {
+router.post('/posts/:post/comments',function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
-  comment.author = req.payload.username;
+  //comment.author = req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
