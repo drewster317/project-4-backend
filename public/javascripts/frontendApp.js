898e5ecd4ae59ee4fb6e0875a3812887c3501b58
Drewster317 <drewster317@gmail.com>
2015-12-17 10:00:27 -0500

    MODIFIED app.factory to get token of user to be able to upvote and add comment

--- public/javascripts/frontendApp.js
+++ public/javascripts/frontendApp.js
@@ -163,26 +163,30 @@ app.factory('posts', ['$http', 'auth', function($http, auth){
 
   o.create = function(post) {
     return $http.post('/posts', post, {
-      header
+      headers: {Authorization: 'Bearer '+auth.getToken()}
     }).success(function(data){
       o.posts.push(data);
     });
   };
 
   o.upvote = function(post) {
-    return $http.put('/posts/' + post._id + '/upvote')
-    .success(function(data){
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
       post.upvotes += 1;
     });
   };
 
   o.addComment = function(id, comment) {
-    return $http.post('/posts/' + id + '/comments', comment);
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+      });
   };
 
   o.upvoteComment = function (post, comment) {
-    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
-    .success(function (data) {
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function (data) {
       comment.votes += 1;
     });
   };
