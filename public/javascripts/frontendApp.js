064d6712ef77f4710a0a056f99cbc5ce95c52091
Drewster317 <drewster317@gmail.com>
2015-12-17 09:20:35 -0500

    CREATED HTML forms for REGISTER and LOGIN

--- public/javascripts/frontendApp.js
+++ public/javascripts/frontendApp.js
@@ -88,6 +88,32 @@ return auth;
 
 
 
+.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+
+
+
 
 
 app.factory('posts', ['$http', function($http){
